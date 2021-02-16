e3aebc88c6935360bfed42b28ba44f911a45b0fa
svarthrafn <79121737+svarthrafn@users.noreply.github.com>
2021-02-15 21:09:23 -0500

    Add files via upload

---
+++ fortuneteller.py
@@ -0,0 +1,64 @@
+prompt1 = input("Shall I tell your fortune? y/n ")
+if (prompt1 == "y", "yes", "Y", "YES", "Yes"):
+        print: user_choice_colors = input("Choose a color: yellow, pink, red, or green: ")
+        if (user_choice_colors == "yellow"):
+            number = input("Choose a number: 2, 3, 6, 7 :")
+            if (number == "2"):
+                print ("You will fall into a hole")
+            elif (number == "3"):
+                print ("You will turn green")
+            elif (number == "6"):
+                print ("I like toast")
+            elif (number == "7"):
+                print ("You will find a cow")
+            else:
+                print ("That is not an appropriate response!")
+        elif (user_choice_colors == "pink"):
+            number = input("Choose a number: 1, 4, 5, 8 :")
+            if (number == "1"):
+                print ("It will be your birthday in less than a year")
+            elif (number == "4"):
+                print ("Terrible things will happen to your newt")
+            elif (number == "5"):
+                print ("An ostrich will make a way into your life")
+            elif (number == "8"):
+                print ("You will not go to jail today")
+            else:
+                print ("That is not an appropriate response!")
+        elif (user_choice_colors == "red"):
+            number = input("Choose a number: 0, 9, 11, 13 :")
+            if (number == "0"):
+                print ("You will acquire a taste for strawberry jam")
+            elif (number == "9"):
+                print ("Your winning lottery numbers are 3,12,14,28,32")
+            elif (number == "11"):
+                print ("You will meet a ghost")
+            elif (number == "13"):
+                print ("Terrible things will happen to your first grade teacher")
+            else:
+                print ("That is not an appropriate response!")
+        elif (user_choice_colors == "green"):
+            number = input("Choose a number: 10, 12, 14, 15 :")
+            if (number == "10"):
+                print ("Ants will know your name")
+            elif (number == "12"):
+                print ("You will forget how to swim")
+            elif (number == "14"):
+                print ("You have lost the game")
+            elif (number == "15"):
+                print ("Turkey")
+            else:
+                print ("That is not an appropriate response!")
+        else: 
+            print ("That's not what I asked!")
+else:
+    print ("ok, maybe some other time")
+
+#if (user_choice_yellow_numbers == "2"):
+#     input("Choose a number: 2, 3, 6, 7: ")
+# if (user_choice_yellow2 == "2"):
+#     print("You have been granted a health spell. ")
+
+
+
+
